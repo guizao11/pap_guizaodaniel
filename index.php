ceca01ac153a97b4cc565bba051113102fa4a38e
Daniel <daniel.ribeiro28335@al.aememmartins.pt>
2025-03-18 16:53:13 +0000

    formulario

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
         <title> Gestão de Negócios  </title>
         <nav>
             <a href="index.php"> Dashboard </a>
-            <a href = "produtos.php"> Produtos </a>
-            <a href = "vendas.php"> Vendas </a>
+            <a href ="produtos.php"> Produtos </a>
+            <a href ="vendas.php"> Vendas </a>
         </nav>
     </header>
 
     <section id="dashboard">
-        <h2> Bem-vindo ao Painel de Gestão </h2>
+        <h2> Bem-vindo à Business Up </h2>
         <p> Aqui poderá gerir os seus produtos e vendas. </p>
     </section>
 
